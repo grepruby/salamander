181750e70da20314803446231f5072df5ab38701
Dinesh <bharat.dhakd@gmail.com>
2013-06-03 20:52:35 +0530

    admin pages

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.13'
+gem 'rails_admin'
 gem 'pg'
+
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+   # Feel free to change '/admin' to any namespace you need.
   authenticated :user do
     root :to => 'home#index'
   end
